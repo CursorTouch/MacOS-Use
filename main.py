3221420069a39047f37b90398350a73ccd8c7af8
jeomon <jeogeoalukka@gmail.com>
2026-05-13 11:44:59 +0530

    refactor: update LLM provider to Mistral and configure Desktop settings in notebook

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from macos_use.providers.anthropic import ChatAnthropic
-from macos_use.providers.ollama import ChatOllama
-from macos_use.providers.groq import ChatGroq
+from macos_use.providers.mistral import ChatMistral
 from macos_use.agent import Agent
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def main():
-    llm=ChatAnthropic(model="claude-haiku-4-5")
+    # llm=ChatAnthropic(model="claude-haiku-4-5")
     # llm=ChatOllama(model="qwen3-vl:4b")
     # llm=ChatGroq(model="openai/gpt-oss-120b")
+    # llm=ChatNvidia(model="nvidia/nemotron-3-super-120b-a12b")
+    llm=ChatMistral(model="mistral-medium-3-5")
+
     agent = Agent(llm=llm,log_to_file=True,auto_minimize=True)
     query=input("Enter your query: ")
     result=agent.invoke(query)
